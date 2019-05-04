8b8725cd1282f8822fe51ed22c185aa94ff73bb9
nenirodrigues <nenirodrigues@hotmail.com>
2019-05-04 21:50:01 +0100

    contact page first submit

---
+++ contact.php
@@ -0,0 +1,75 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+   
+	<!--Custom styles-->
+    <link rel="stylesheet" type="text/css" href="general.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/main.css">
+  </head>
+  <body>
+  <div class="container">
+    <div class="d-flex justify-content-center h-100">
+		<div class="card2">
+			<?php include "menu.php"; ?>
+            <div class="container2">
+  
+    <div class="container2">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="google-map"><iframe frameborder="0" style="border:0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2381.5538859540884!2d-6.262967584161359!3d53.35124157998001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48670e869f3ecf2f%3A0x52351ba3ab63e170!2sO&#39;Connell+Street+Upper%2C+Dublin!5e0!3m2!1spt-PT!2sie!4v1556821224553!5m2!1spt-PT!2sie" width="100%" height="100%" allowfullscreen=""></iframe></div>
+            </div>
+            <div class="col-md-6">
+                <h2>
+                    Drop a Line
+                </h2>
+                <div>
+                    <div class="icon-block pb-3">
+                        <span class="icon-block__icon">
+                            <span class="mbri-letter mbr-iconfont" media-simple="true"></span>
+                        </span>
+                        <h4 class="icon-block__title align-left mbr-fonts-style display-5">
+                            Don't hesitate to contact us
+                        </h4>
+                    </div>
+                    <div class="icon-contacts pb-3">
+                        <h5 class="align-left mbr-fonts-style display-7">
+                            5, O'Connel Street Lower, Dublin 1, Ireland.
+                        </h5>
+                        <p class="mbr-text align-left mbr-fonts-style display-7">
+                            Phone: +1 (0) 000 0000 001 <br>
+                            Email: <a href="/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="ef96809a9d8a828e8683af828e8683c18c8082">[email&#160;protected]</a>
+                        </p>
+                    </div>
+                </div>
+                <div data-form-type="formoid">
+                    <div data-form-alert="" hidden="">
+                        Thanks for filling out the form!
+                    </div>
+                    <form class="block mbr-form" action="https://mobirise.com/" method="post" data-form-title="Mobirise Form"><input type="hidden" data-form-email="true" value="aSFHM2ssrQz9mN7Fy7H6z2TAXicPkwvkHb4Ls9cAh3dbjscChui1OIZ8KdH27SakT7PEgf5TmnKB/RUmB8n2Z3oEMa9fcSI07swTbNyA6kEQ7nmVOieddh4LWV6bPAHA">
+                        <div class="row">
+                            <div class="col-md-6 multi-horizontal" data-for="name">
+                                <input type="text" class="form-control input" name="name" data-form-field="Name" placeholder="Your Name" required="" id="name-form4-4v">
+                            </div>
+                            <div class="col-md-6 multi-horizontal" data-for="phone">
+                                <input type="text" class="form-control input" name="phone" data-form-field="Phone" placeholder="Phone" required="" id="phone-form4-4v">
+                            </div>
+                            <div class="col-md-12" data-for="email">
+                                <input type="text" class="form-control input" name="email" data-form-field="Email" placeholder="Email" required="" id="email-form4-4v">
+                            </div>
+                            <div class="col-md-12" data-for="message">
+                                <textarea class="form-control input" name="message" rows="3" data-form-field="Message" placeholder="Message" style="resize:none" id="message-form4-4v"></textarea>
+                            </div>
+                            <div class="input-group-btn col-md-12" style="margin-top: 10px;">
+                                <button href="" type="submit" class="btn btn-primary btn-form display-4">SEND MESSAGE</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+              </body>
+</html>
